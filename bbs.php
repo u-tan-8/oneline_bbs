81cc5492adb1b2ed23439c2e231c97f19a3d40b8
yuuji.maeda <snowtears_y@yahoo.co.jp>
2020-07-26 00:35:11 +0900

    SQL_preservation

--- bbs.php
+++ bbs.php
@@ -12,7 +12,42 @@ if (mysqli_connect_errno()) {
  
 echo 'データベース接続成功';
 
+//POSTなら保存処理実行
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //名前が正しく入力されているかチェック
+    //issetは値が入っているか確認
+    //strlenは文字列を確認
+    $name = null;
+    if(!isset($_POST['name']) || !strlen($_POST['name'])) {
+        $errors['name'] = '名前を入力してください';
+    } else if (strlen($_POST['name']) > 40 ) {
+        $errors['name'] = '名前は40文字以内で入力してください';
+    } else {
+        $name = $_POST['name'];
+    }
+}
 
+//ひとことが正しく入力されているかチェック
+$comment = null;
+if(!isset($_POST['comment']) || !strlen($_POST['comment'])) {
+    $errors['comment'] = 'ひとことを入力してください';
+} else if (strlen($_POST['comment']) > 200 ) {
+    $errors['comment'] = 'ひとことは200文字以内で入力してください';
+} else {
+    $comment = $_POST['comment'];
+}
+
+//エラーがなければ保存
+if(count($errors) === 0) {
+    //保存するためのSQL文
+    $sql = "INSERT INTO `post` (`name`,`comment`,`created_at`) VALUES ('"
+        . mysqli_real_escape_string ($link, $name) . "', '"
+        . mysqli_real_escape_string ($link, $comment) . "', '"
+        .date('Y-m-d H:i:s') . "')";
+
+    //保存する
+    mysqli_query($link, $sql);
+}
 
 ?>
 
